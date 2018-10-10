2718a14e4a4be9d6ab92e914057f77e151d50367
laptop\Ebbe Vang <ev@evang.dk>
2018-10-10 12:52:03 +0200

    Update src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        // comment from laptop pc
+        // comment from ebbe vang
     }
 }
